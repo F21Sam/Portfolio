ed33627dfff31e61d4e02ff0a780547296db2cfb
Famamou <samsfanta@gmail.com>
2025-01-29 11:26:13 +0100

    essai

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <h2 className="text-marron text-4xl font-bold">Bonjour, je suis Fanta Samassa</h2>
         <p className="text-nude text-lg mt-4">Développeuse web spécialisée en React, TypeScript et JavaScript.</p>
         <img
-          src="/portrait.jpg" 
+          src="/IMG_6771.jpg" 
           alt="Fanta Samassa"
           className="w-48 h-48 rounded-full mx-auto mt-6"
         />
